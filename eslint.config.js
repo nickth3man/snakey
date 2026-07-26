2dc422b696f621b1e5fd1d6ce82c71ad2acd7cf8
Nicolas Alexander <nickth3man@gmail.com>
2026-07-25 20:16:06 -0400

    chore: add ESLint + typescript-eslint flat config | M-02
    Suppress no-non-null-assertion pending L-05; wire lint script into package.json.

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      // L-05: removal of non-null assertions tracked separately
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+);
