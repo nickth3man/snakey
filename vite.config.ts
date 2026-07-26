f1d0250338356f06b87eab8ff94593198d3636f3
Nicolas Alexander <nickth3man@gmail.com>
2026-07-25 21:38:46 -0400

    Add GitHub Pages deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/snakey/" : "/",
   server: {
     open: true,
   },
-});
+}));
